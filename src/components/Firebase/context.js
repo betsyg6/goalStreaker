9f314408d8e4a6f1ef7432f4c362cca94f6327cf
betsyg6 <grotoned@gmail.com>
2021-01-14 12:53:54 -0500

    feat: firebase connected to react components

---
+++ src/components/Firebase/context.js
@@ -0,0 +1,3 @@
+import react from 'react';
+const FirebaseContext = react.createContext(null);
+export default FirebaseContext;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<FirebaseContext.Provider value={new Firebase()}>
+		<App />
+	</FirebaseContext.Provider>,
+	document.getElementById('root')
 );
-
-
