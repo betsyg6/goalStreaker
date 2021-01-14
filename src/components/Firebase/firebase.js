50aff7fcf3fdbf02d85497a426ed51873c1ae21f
betsyg6 <grotoned@gmail.com>
2021-01-14 14:57:17 -0500

    feat: authentication interface for react components that connect to firebase API completed

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,5 @@
-import { configure } from '@testing-library/react';
 import app from 'firebase.app';
+import 'firebase/auth';
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_API_KEY,
@@ -14,7 +14,26 @@ const firebaseConfig = {
 class Firebase {
 	constructor() {
 		app.initializeApp(config);
+
+		this.auth = app.auth();
 	}
+
+	//authentication methods
+	doCreateUserWithEmailAndPassword = (email, password) => {
+		this.auth.createUserWithEmailAndPassword(email, password);
+	};
+
+	doSignInWithEmailAndPassword = (email, password) => {
+		this.auth.signInWithEmailAndPassword(email, password);
+	};
+
+	doSignOut = () => this.auth.signOut();
+
+	//password stuff for authenticated users
+	doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+
+	doPasswordUpdate = (password) =>
+		this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
