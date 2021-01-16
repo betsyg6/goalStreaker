5f8f1de8e614e2bc66f94e539125aec850acd27d
betsyg6 <grotoned@gmail.com>
2021-01-16 13:04:29 -0500

    feat: material ui for nav

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,6 +3,17 @@ import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import SignOut from '../SignOut';
 import { AuthUserContext } from '../Session';
+//material ui
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+// import ListSubheader from '@material-ui/core/ListSubheader';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+// import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PeopleIcon from '@material-ui/icons/People';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import LayersIcon from '@material-ui/icons/Layers';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 
 const Navigation = () => (
 	<div>
@@ -13,34 +24,55 @@ const Navigation = () => (
 );
 
 const NavigationAuth = () => (
-	<ul>
-		<li>
-			<Link to={ROUTES.LANDING}>Landing</Link>
-		</li>
-		<li>
-			<Link to={ROUTES.HOME}>Home</Link>
-		</li>
-		<li>
-			<Link to={ROUTES.ACCOUNT}>Account</Link>
-		</li>
-		<li>
-			<Link to={ROUTES.ADMIN}>Admin</Link>
-		</li>
-		<li>
+	<div>
+		<ListItem button component={Link} to={ROUTES.LANDING}>
+			<ListItemIcon>
+				<DashboardIcon />
+			</ListItemIcon>
+			<ListItemText primary='Landing' />
+		</ListItem>
+		<ListItem button component={Link} to={ROUTES.HOME}>
+			<ListItemIcon>
+				<PeopleIcon />
+			</ListItemIcon>
+			<ListItemText primary='Home' />
+		</ListItem>
+		<ListItem button component={Link} to={ROUTES.ACCOUNT}>
+			<ListItemIcon>
+				<BarChartIcon />
+			</ListItemIcon>
+			<ListItemText primary='Account' />
+		</ListItem>
+		<ListItem button component={Link} to={ROUTES.ADMIN}>
+			<ListItemIcon>
+				<AssignmentIcon />
+			</ListItemIcon>
+			<ListItemText primary='Admin' />
+		</ListItem>
+		<ListItem button>
+			<ListItemIcon>
+				<LayersIcon />
+			</ListItemIcon>
 			<SignOut />
-		</li>
-	</ul>
+		</ListItem>
+	</div>
 );
 
 const NavigationNonAuth = () => (
-	<ul>
-		<li>
-			<Link to={ROUTES.LANDING}>Landing</Link>
-		</li>
-		<li>
-			<Link to={ROUTES.SIGN_IN}>Sign In</Link>
-		</li>
-	</ul>
+	<div>
+		<ListItem button component={Link} to={ROUTES.LANDING}>
+			<ListItemIcon>
+				<DashboardIcon />
+			</ListItemIcon>
+			<ListItemText primary='Landing' />
+		</ListItem>
+		<ListItem button component={Link} to={ROUTES.SIGN_IN}>
+			<ListItemIcon>
+				<PeopleIcon />
+			</ListItemIcon>
+			<ListItemText primary='Sign In' />
+		</ListItem>
+	</div>
 );
 
 export default Navigation;
