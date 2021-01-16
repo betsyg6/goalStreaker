75a556f69b1b5a10010ee86bf166bea1a0090a8f
betsyg6 <grotoned@gmail.com>
2021-01-16 17:00:44 -0500

    feat: mostly completed basic app styling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,22 +11,47 @@ import Admin from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
 import { withAuthentication } from '../Session';
+//material ui
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const App = () => (
-	<Router>
-		<div>
-			<Navigation />
-			<hr />
+const useStyles = makeStyles((theme) => ({
+	icon: {
+		marginRight: theme.spacing(2),
+	},
+	heroContent: {
+		backgroundColor: theme.palette.background.paper,
+		padding: theme.spacing(8, 0, 6),
+	},
+	container: {
+		display: 'flex',
+		height: '100%',
+		width: '100%',
+	},
+}));
 
-			<Route exact path={ROUTES.LANDING} component={Landing} />
-			<Route path={ROUTES.SIGN_UP} component={SignUp} />
-			<Route path={ROUTES.SIGN_IN} component={SignIn} />
-			<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
-			<Route path={ROUTES.HOME} component={Home} />
-			<Route path={ROUTES.ACCOUNT} component={Account} />
-			<Route path={ROUTES.ADMIN} component={Admin} />
-		</div>
-	</Router>
-);
+const App = () => {
+	const classes = useStyles();
+
+	return (
+		<Router>
+			<div className={classes.container}>
+				<Navigation />
+				{/* Hero unit */}
+				<div className={classes.heroContent}>
+					<Container>
+						<Route exact path={ROUTES.LANDING} component={Landing} />
+						<Route path={ROUTES.SIGN_UP} component={SignUp} />
+						<Route path={ROUTES.SIGN_IN} component={SignIn} />
+						<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+						<Route path={ROUTES.HOME} component={Home} />
+						<Route path={ROUTES.ACCOUNT} component={Account} />
+						<Route path={ROUTES.ADMIN} component={Admin} />
+					</Container>
+				</div>
+			</div>
+		</Router>
+	);
+};
 
 export default withAuthentication(App);
