35ce6b05a51f9268a7937428d7362abdcc29d64d
betsyg6 <grotoned@gmail.com>
2021-01-16 12:32:01 -0500

    feat: material ui for password forgot

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
+//material ui
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 const PasswordForget = () => (
 	<div>
-		<h1>Password Forget</h1>
 		<PasswordForgetForm />
 	</div>
 );
@@ -48,20 +55,45 @@ class PasswordForgetFormBase extends Component {
 		const isInvalid = email === '';
 
 		return (
-			<form onSubmit={this.onSubmit}>
-				<input
-					name='email'
-					value={this.state.email}
-					onChange={this.onChange}
-					type='text'
-					placeholder='Email Address'
-				/>
-				<button disabled={isInvalid} type='submit'>
-					Reset My Password
-				</button>
+			<Container component='main' maxWidth='xs'>
+				<CssBaseline />
+				<div>
+					<Avatar>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component='h1' variant='h5'>
+						Forgot Your Password?
+					</Typography>
+					<form onSubmit={this.onSubmit} noValidate>
+						<TextField
+							variant='outlined'
+							margin='normal'
+							required
+							fullWidth
+							id='email'
+							label='Email Address'
+							name='email'
+							value={email}
+							onChange={this.onChange}
+							autoComplete='email'
+							autoFocus
+						/>
 
-				{error && <p>{error.message}</p>}
-			</form>
+						<Button
+							type='submit'
+							disabled={isInvalid}
+							fullWidth
+							variant='contained'
+							color='primary'
+						>
+							Reset Password
+						</Button>
+						<Grid container justify='flex-end'>
+							<Grid item>{error && <p>{error.message}</p>}</Grid>
+						</Grid>
+					</form>
+				</div>
+			</Container>
 		);
 	}
 }
