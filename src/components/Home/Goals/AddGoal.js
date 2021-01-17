590ac142597ae2221d26ce6daef75780e61de49f
betsyg6 <grotoned@gmail.com>
2021-01-17 17:34:39 -0500

    feat: add goal to firebase db done

--- src/components/Home/Goals/AddGoal.js
+++ src/components/Home/Goals/AddGoal.js
@@ -2,10 +2,11 @@
 
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-// import * as ROUTES from '../../constants/routes';
+import * as ROUTES from '../../../constants/routes';
 // import { FirebaseContext } from 'react-router-dom';
 import { withFirebase } from '../../Firebase';
 import { compose } from 'recompose';
+import { AuthUserContext } from '../../Session';
 //material ui
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -35,18 +36,21 @@ class AddGoalFormBase extends Component {
 		this.state = { ...INITIAL_STATE };
 	}
 
-	onSubmit = (event) => {
+	onSubmit = (event, authUser) => {
 		const { title, daysCompleted, totalDays } = this.state;
 		//not sure what goes here...would need to add the goal to the db and have it be associated to the user
-		// .then((authUser) => {
-		// this.setState({ ...INITIAL_STATE });
-		// this.props.history.push(ROUTES.HOME);
-		// })
-		// .catch((error) => {
-		// 	this.setState({ error });
-		// });
+		this.props.firebase
+			.goal(authUser.uid)
+			.set({ title, daysCompleted, totalDays })
+			.then(() => {
+				this.setState({ ...INITIAL_STATE });
+				this.props.history.push(ROUTES.HOME);
+			})
+			.catch((error) => {
+				this.setState({ error });
+			});
 
-		this.setState({ ...INITIAL_STATE });
+		// this.setState({ ...INITIAL_STATE });
 		// this.props.history.push(ROUTES.HOME);
 
 		event.preventDefault();
@@ -63,71 +67,79 @@ class AddGoalFormBase extends Component {
 
 		console.log(this.state);
 		return (
-			<Container component='main' maxWidth='xs'>
-				<CssBaseline />
-				<div>
-					<Typography component='h1' variant='h5'>
-						New Goal
-					</Typography>
-					<form noValidate onSubmit={this.onSubmit}>
-						<Grid container spacing={2}>
-							<Grid item xs={12} sm={6}>
-								<TextField
-									autoComplete='title'
-									name='title'
-									variant='outlined'
-									value={title}
-									required
-									onChange={this.onChange}
+			<AuthUserContext.Consumer>
+				{(authUser) => (
+					<Container component='main' maxWidth='xs'>
+						<CssBaseline />
+						<div>
+							<Typography component='h1' variant='h5'>
+								New Goal
+							</Typography>
+
+							<form
+								noValidate
+								onSubmit={(event) => this.onSubmit(event, authUser)}
+							>
+								<Grid container spacing={2}>
+									<Grid item xs={12} sm={6}>
+										<TextField
+											autoComplete='title'
+											name='title'
+											variant='outlined'
+											value={title}
+											required
+											onChange={this.onChange}
+											fullWidth
+											id='title'
+											label='Goal Title'
+											autoFocus
+										/>
+									</Grid>
+									<Grid item xs={12} sm={6}>
+										{/* change this to something you can't edit on this page */}
+										<TextField
+											variant='outlined'
+											required
+											fullWidth
+											value={daysCompleted}
+											id='daysCompleted'
+											label='Days Completed'
+											onChange={this.onChange}
+											name='daysCompleted'
+											autoComplete='daysCompleted'
+										/>
+									</Grid>
+									<Grid item xs={12}>
+										<TextField
+											variant='outlined'
+											required
+											fullWidth
+											value={totalDays}
+											name='totalDays'
+											label='Total Days'
+											id='totalDays'
+											onChange={this.onChange}
+											autoComplete='totalDays'
+										/>
+									</Grid>
+								</Grid>
+								<Button
+									disabled={isInvalid}
+									type='submit'
 									fullWidth
-									id='title'
-									label='Goal Title'
-									autoFocus
-								/>
-							</Grid>
-							<Grid item xs={12} sm={6}>
-								{/* change this to something you can't edit on this page */}
-								<TextField
-									variant='outlined'
-									required
-									fullWidth
-									value={daysCompleted}
-									id='daysCompleted'
-									label='Days Completed'
-									onChange={this.onChange}
-									name='daysCompleted'
-									autoComplete='daysCompleted'
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<TextField
-									variant='outlined'
-									required
-									fullWidth
-									value={totalDays}
-									name='totalDays'
-									label='Total Days'
-									id='totalDays'
-									onChange={this.onChange}
-									autoComplete='totalDays'
-								/>
-							</Grid>
-						</Grid>
-						<Button
-							disabled={isInvalid}
-							type='submit'
-							fullWidth
-							variant='contained'
-							color='primary'
-						>
-							Sign Up
-						</Button>
-						<Grid container justify='flex-end'>
-							<Grid item>{error && <p>{error.message}</p>}</Grid>
-						</Grid>
-					</form>
-				</div>
-			</Container>
+									variant='contained'
+									color='primary'
+								>
+									Sign Up
+								</Button>
+								<Grid container justify='flex-end'>
+									<Grid item>{error && <p>{error.message}</p>}</Grid>
+								</Grid>
+							</form>
+						</div>
+					</Container>
+				)}
+			</AuthUserContext.Consumer>
 		);
 	}
 }
