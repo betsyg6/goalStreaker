bd0196fe534e5ef9aca8b07b0525a0aa723f8d4b
betsyg6 <grotoned@gmail.com>
2021-01-21 18:27:14 -0500

    feat: redirect after signout

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
-- change the text input fields for the numbers
 - redirect after signout
 - sign out icon in top right corner
-- add a footer -- make the footer stick to the bottom
+- toast notis for adding to goal
 
 - overall styling
 - prettier landing page
--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+import { Link } from 'react-router-dom';
 
 const SignOut = ({ firebase }) => {
 	return (
-		<button type='button' onClick={firebase.doSignOut}>
-			Sign Out
-		</button>
+		<Link to={ROUTES.LANDING}>
+			<button type='button' onClick={firebase.doSignOut}>
+				Sign Out
+			</button>
+		</Link>
 	);
 };
 
